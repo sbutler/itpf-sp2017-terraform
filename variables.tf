85cdf40f40bd762768d6538f4f1c4e53c587d1ae
Stephen J. Butler <stephen.butler@gmail.com>
2017-05-24 20:26:49 -0500

    Empty variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# ===================================================================
+# Variables
+# ===================================================================
+
+# Documentation on variables:
+# https://www.terraform.io/docs/configuration/variables.html
+#
+# Documentation on using them and string interpolation:
+# https://www.terraform.io/docs/configuration/interpolation.html
+
+# Example:
+# variable "foo" {
+#     type = "string"
+#     description = "A little variable named 'foo'."
+# }
