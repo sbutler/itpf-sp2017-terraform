a931f8c98a6404268be85d43ca661187fdb5994a
Stephen J. Butler <stephen.butler@gmail.com>
2017-05-24 20:33:29 -0500

    Empty outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+# ===================================================================
+# Outputs
+# ===================================================================
+
+# Outputs will be shown at the end of every terraform apply, and also on demand
+# by running terraform output. In more advanced usage, outputs are also
+# available in remote state for use in other terraform configurations.
+#
+# Documentation on outputs:
+# https://www.terraform.io/docs/configuration/outputs.html
+
+# Example:
+# output "foo" {
+#     value = "${var.foo}"
+# }
